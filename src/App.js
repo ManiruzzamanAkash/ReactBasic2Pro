b41e8523a9415b1a55ecfb1375d4f37228ce6702
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-10-31 01:55:00 +0600

    Day 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Welcome to React World !! New 
-        </p>
+        <p>Welcome to React World !! New</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <>
+      Welcome
+      <h2>Welcome</h2>
+    </>
+  );
+};
+
+export default Welcome;
