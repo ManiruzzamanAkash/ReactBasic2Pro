ee778d4c247c316090cbc53827f6bbaa709d06e4
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-04-18 19:34:38 +0600

    clear bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from "./reportWebVitals";
 
-/** Components */
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
