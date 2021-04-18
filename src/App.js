3c0697df9549a5706d52bd84d16a2c9ee8029e1d
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-04-18 19:33:23 +0600

    clear the project

--- src/App.js
+++ src/App.js
@@ -1,65 +1,11 @@
 
-import React, { useState } from "react";
-import CounterClass from "./components/CounterClass";
-import CounterFunctional from "./components/CounterFunctional";
-import CounterNew from "./components/CounterNew";
-import TaskList from "./components/TaskList";
-import Test from "./components/Test";
+import React from "react";
 
 const App = () => {
-  const name = "Maniruzzaman Akash";
-
-  const [address, setAddress] = useState({
-    village: 'Mohakhali 87/ka',
-    city: 'Dhaka',
-    country: 'Bangladesh'
-  });
-
-  const [programmingLanguages, setProgrammingLanguages] = useState([
-    'Java',
-    'PHP',
-    'C#',
-  ]);
-
-  const changeCityFromParent = () => {
-    setAddress({
-      village: 'Mohakhali 87/ka',
-      city: address.city === 'Dhaka' ? 'Another City' : 'Dhaka',
-      country: 'Bangladesh'
-    })
-  }
-
-  const changeCityFromParentDynamic = (value) => {
-    setAddress({
-      village: 'Mohakhali 87/ka',
-      city: address.city === 'Dhaka' ? value : 'Dhaka',
-      country: 'Bangladesh'
-    })
-  }
-
-
-
   
-  const [counter, setCounter] = useState(0);
-
   return (
     <>
-      <h2>Tutorial 15 - Component Communication</h2>
-      <Test
-        name={name}
-        myAddress={address}
-        pl={programmingLanguages}
-        changeCityFromParent={changeCityFromParent}
-        changeCityFromParentDynamic={changeCityFromParentDynamic}
-        setProgrammingLanguages={setProgrammingLanguages}
-      />
-
-      <button onClick={changeCityFromParent}>
-        Change City
-      </button>
-      <TaskList />
-       <TaskList />
-      <CounterNew counter={counter} setCounter={setCounter}/>
+      <h2>React App</h2>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from "./reportWebVitals";
 
 /** Components */
-// import App from "./App";
-import Day17 from "./components/Day17/Day17";
+import App from "./App";
 
-ReactDOM.render(<Day17 />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
