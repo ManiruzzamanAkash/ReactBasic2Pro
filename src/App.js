9c69856b4806e4e64e02456814e654cc088df145
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-11-14 14:15:54 +0600

    Setup ready for Day 25 - React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 
-import React, { useState } from "react";
-import "./assets/App.css";
-import Value from "./components/Value";
-import Context from "./context/Context";
+import React from "react";
 
 const App = () => {
-  const [data, setData] = useState({
-    value: 10,
-    tasks: [
-      'Caculation Task 1',
-      'Caculation Task 2',
-      'Caculation Task 3',
-    ],
-    auth: false,
-  });
-
   return (
-    <Context.Provider value={{ state: data, setData }}>
       <div className="app">
-        <h2>React Context API Example</h2>
-        <hr />
-        <Value />
+        <h2>React Router Project</h2>
       </div>
-    </Context.Provider>
   )
 }
 
