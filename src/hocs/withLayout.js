41bfd31016cc959160cbb8b084572b31fd40184d
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-12-06 07:50:10 +0600

    Updated

--- src/hocs/withLayout.js
+++ src/hocs/withLayout.js
@@ -3,8 +3,6 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const withLayout = (PageComponent) => {
-    const [count2, setCounter2] = useState(100);
-
     return function Page() {
         const [count, setCounter] = useState(10);
         return (
